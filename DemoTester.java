5185502e7962c126a9ffbb113b9e3ca607e34c61
Sasha-M-Motielall <smotielall20@stuy.edu>
2020-12-01 01:17:04 -0500

    findOverLap method tested

--- DemoTester.java
+++ DemoTester.java
@@ -7,5 +7,13 @@ public class DemoTester {
     System.out.println(words);
     Demo.removeDuplicates(words);
     System.out.println(words);
+
+    SuperArray overlaps = new SuperArray();
+    overlaps.add("a");  overlaps.add("b");  overlaps.add("c");
+    overlaps.add("d");  overlaps.add("e");  overlaps.add("f");
+    SuperArray overlaps2 = new SuperArray();
+    overlaps2.add("e"); overlaps2.add("f");  overlaps2.add("g");
+    overlaps2.add("h"); overlaps2.add("i");  overlaps2.add("j");
+    System.out.println(Demo.findOverLap(overlaps,overlaps2));
   }
 }
